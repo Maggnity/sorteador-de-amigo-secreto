796c6cfacb0d3d0b458957f3c71d5c9bc4332e5a
Fernando Vinícius <mag.nando07@gmail.com>
2022-06-07 11:14:31 -0300

    Adicionando teste de formulário

---
+++ src/components/Formulario.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import * as React from 'react';
+
+//jest
+
+test('Quando o input está vazio, novos participantes não podem ser adicionados', () => {
+    
+    render(<Formulario />)
+    
+    // encontrar no DOM o input;
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    
+    // encontrar o botão;
+    const botao = screen.getByRole('button')
+
+
+    // garantir que o input esteja no documento;
+    expect(input).toBeInTheDocument()
+
+    // garantir que o botão esteja desabilitado;
+    expect(botao).toBeDisabled()
+})
