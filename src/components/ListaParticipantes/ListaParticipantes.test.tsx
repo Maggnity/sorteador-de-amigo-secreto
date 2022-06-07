e22bca2de0c5a503dfcf8cb9abe67490ba4a7fd1
Fernando Vinícius <mag.nando07@gmail.com>
2022-06-07 19:45:22 -0300

    Adicionando lista de participantes/Usando mock em teste para dublar a lista de participantes

---
+++ src/components/ListaParticipantes/ListaParticipantes.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+import * as React from 'react';
+import { RecoilRoot } from 'recoil';
+import { useListaDeParticipantes } from '../../state/hook/useListaDeParticipantes';
+import ListaParticipantes from './ListaParticipantes';
+
+jest.mock('../../state/hook/useListaDeParticipantes', () => {
+    return {
+        useListaDeParticipantes: jest.fn()
+    }
+});
+
+describe('Uma lista vazia de participantes', () => {
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue([])
+    })
+    test('deve ser renderizada sem elementos', () => {
+        render(
+            <RecoilRoot>
+                <ListaParticipantes />
+            </RecoilRoot>
+        );
+    
+        const itens = screen.queryAllByRole('listitem');
+        expect(itens).toHaveLength(0)
+    })
+})
+
+describe('Uma lista preenchida de participantes', () => {
+    const participantes = ['Luana', 'Fernando', 'Luke']
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+    });
+    test('deve ser renderizada sem elementos', () => {
+        render(
+            <RecoilRoot>
+                <ListaParticipantes />
+            </RecoilRoot>
+        );
+    
+        const itens = screen.queryAllByRole('listitem');
+        expect(itens).toHaveLength(participantes.length);
+    })
+})
